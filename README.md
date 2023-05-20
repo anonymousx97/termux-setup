5a4a06c6bbf946f99c5e675053973cba4d1e940d
anonymousx97 <88324835+anonymousx97@users.noreply.github>
2023-05-20 13:31:37 +0530

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Termux Setup
+
+### A script to auto-install few packages and customise termux.
+<p align="center"><img src="https://raw.githubusercontent.com/anonymousx97/termux-setup/main/preview.png"></p>  
+
+>It was made because Ryuk was ~~LAZY~~ tired of setting up termux again and again. XD
+
+### Installation
+  ```bash
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/anonymousx97/termux-setup/main/setup.sh)"
+  ```
+### Credits 
+* [anonymousx97](https://github.com/anonymousx97) for _setup.sh_  
+* [mayTrmux](https://github.com/mayTermux) for original _color.properties & rxfetch_
---
+++ setup.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+
+# By Ryuk Github | TG : [ anonymousx97 ]
+
+red="\033[1;31m"
+white="\033[1;37m"
+green="\033[1;32m"
+defaults=false
+
+start(){
+    clear
+    local menu="${green}Main Menu:${white}"
+    local options="
+  1. Install packages
+  2. Customize Termux
+  3. Both (Uses Presets for customisation)
+  4. Exit
+"
+    both(){
+        defaults=true
+        package_setup
+        customize
+    }
+    declare -A start_options
+    start_options[1]="package_setup"
+    start_options[2]="customize"
+    start_options[3]="both"
+    start_options[4]="exit_"
+    ask "${options}" "${menu}" "start_options"
+}
+
+
+
+customize(){
+    clear
+    local menu="${green}Customisation Menu:${white}"
+    customize_all(){
+        echo -e "${green}Customising All.${white}"
+        setup_aria2
+        setup_rxfetch
+        change_cursor
+        change_ui
+        sleep 2
+        clear
+        rxfetch
+    }
+    if $defaults ; then
+        customize_all
+    else
+        declare -A option_dict
+        option_dict[1]="setup_aria2"
+        option_dict[2]="setup_rxfetch"
+        option_dict[3]="change_cursor"
+        option_dict[4]="change_ui"
+        option_dict[5]="customize_all"
+        option_dict[6]="start"
+        option_dict[7]="exit_"
+        local options="
+  1. Setup Aria2 Shortcut.
+  2. Setup Rxfetch.
+  3. Change Cursor Style.
+  4. Change Background and UI colour.
+  5. All of the above.
+  6. Go back to previous menu.
+  7. Exit
+"
+        ask "${options}" "${menu}" "option_dict"
+    fi
+}
+
+
+
+ask(){
+    local header=" What do you want to do?"
+    local option_text="$1"
+    local menu="$2"
+    local dict_name=$3
+    echo -e "${menu}\n${header}${option_text}"
+    while true;do
+        read -p "> " choice
+        if [ -z $choice ]; then
+            clear
+            echo -e "${menu}\n${header}${red}\n Choose an Option.${white}${option_text}"
+        elif [[ -v ${dict_name}[$choice] ]]; then
+            eval "\${${dict_name}[$choice]}"
+            break 
+        else
+            clear
+            echo -e "${menu}\n${header}${red}\n Invalid Input: ${white}${choice}${option_text}"
+        fi
+    done
+}
+
+package_setup(){
+    # Update Termux Package Repository
+    termux-change-repo
+    yes|pkg upgrade
+    apt update -y 
+    apt upgrade -y
+
+    # Install necessary packages
+    apt install -y \
+        python \
+        python-pip \
+        python-pillow \
+        wget \
+        curl \
+        git \
+        gh \
+        openssh \
+        sshpass \
+        ffmpeg \
+        aria2
+
+    # Update pip packages
+    pip install -U \
+        wheel \
+        setuptools \
+        yt-dlp
+}
+
+
+setup_aria2(){
+    echo -e "\n1. Setting up Aria2 shortcut"
+    if ! [ -f $PATH/arc ] || $defaults ; then
+        arc="#!$PATH/bash\ncd /sdcard/Download\nread -p 'Link or Magnet\n> ' x\naria2c $x"
+        echo -e $arc > $PATH/arc
+        chmod u+x $PATH/arc
+        echo -e "${green}Done.${white}"
+    else
+        echo -e "${red}A script with the name arc exists in ${PATH}${white}"
+    fi
+}
+
+
+setup_rxfetch(){
+    echo -e "\n2. Setting up Rxfetch"
+    curl -s -O --output-dir $PATH https://raw.githubusercontent.com/anonymousx97/termux-setup/main/rxfetch
+    motd="#!$SHELL\nbash rxfetch"
+
+    if [ -f ~/.termux/motd.sh ] && ! $defaults ; then
+        echo -e "${red}A custom start script exists in the path ${HOME}/.termux/motd.sh${white}"
+        echo -e "  Enter 1 to overwrite the current file.\n  Press Enter to skip."
+        read -p "> " prompt_
+        if ! [ $prompt_ ] ||  ! [ $prompt_ == 1 ] ; then
+            echo -e "${green}Skipping motd modification.${white}"
+        else
+            echo -e "${red}Overwriting motd.${white}"
+            echo -e $motd > ~/.termux/motd.sh
+        fi
+    else
+        echo -e $motd > ~/.termux/motd.sh
+    fi
+    echo -e "${green}Done.${white}"
+}
+
+
+change_cursor(){
+    echo -e "\n3. Changing Cursor"
+    local menu="Cursor Menu:"
+    local options="
+  1. Change to ${green}|${white} (bar)
+  2. Change to ${green}_${white} (underscore)
+  3. Change to Default Block style.
+  4. Exit
+"
+    block(){ printf '\e[1 q'; }
+    bar(){ printf '\e[6 q'; }
+    underscore(){ printf '\e[4 q'; }
+
+    if ! $defaults ; then
+        clear
+        declare -A cursor_dict
+        cursor_dict[1]="bar"
+        cursor_dict[2]="underscore"
+        cursor_dict[3]="block"
+        cursor_dict[4]="exit_"
+        ask "${options}" "${menu}" "cursor_dict"
+    else
+        underscore
+    fi
+
+    # Change Blink Rate
+    sed -i "s/terminal-cursor-blink-rate.*/terminal-cursor-blink-rate = 600/" ~/.termux/termux.properties
+    echo -e "${green}Done.${white}"
+}
+
+
+exit_(){
+    echo -e "${green}Exiting...${white}"
+    exit
+}
+
+
+change_ui(){
+    echo -e "Changing UI and background colour."
+    curl -s -O --output-dir $PATH ~/.termux https://raw.githubusercontent.com/anonymousx97/termux-setup/main/colors.properties
+    echo -e "\n${green}Applying Changes.${white}"
+    termux-reload-settings
+    echo -e "${green}Done.${white}"
+}
+
+start
