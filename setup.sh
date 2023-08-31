a9e721cd163b8b7163ec48c0b09e031eccc87001
anonymousx97 <88324835+anonymousx97@users.noreply.github.com>
2023-08-31 19:10:47 +0530

    added .arc_history to save history.

--- setup.sh
+++ setup.sh
@@ -140,8 +140,8 @@ package_setup(){
 setup_aria2(){
     echo -e "\n1. Setting up Aria2 shortcut"
     if ! [ -f $PATH/arc ] || $defaults ; then
-        local arc="#!/data/data/com.termux/files/usr/bin/bash\nread -p 'Link or Magnet\n> ' x\necho\naria2c -d /sdcard/Download --console-log-level=warn --summary-interval=0 --seed-time=0 --file-allocation=none \$x"
-        echo -e $arc > $PATH/arc
+        local arc="#!/data/data/com.termux/files/usr/bin/bash\nread -p 'Link or Magnet\n> ' x\necho -e "\${x}" >> $HOME/.arc_history\necho\naria2c -d /sdcard/Download --console-log-level=warn --summary-interval=0 --seed-time=0 --file-allocation=none \$x"
+        echo -e "${arc}" > $PATH/arc
         chmod u+x $PATH/arc
         echo -e "${green}Done.${white}"
     else
